7f7c17d3b84a72afea1d09916cb87b3a67e11b33
AlexTaures <alextauresdiaz@gmail.com>
2023-10-10 23:33:32 -0600

    mejoras

--- main.py
+++ main.py
@@ -36,21 +36,45 @@ import pyperclip
 
 # Función para generar la clave a partir de la URL ingresada
 def generar_keys():
-    url = entrada.get()
-    url = url.replace("https://sis-", "").replace(".salud.gob.sv/", "").replace("http://sis-", "").replace('a', '4').replace('e', '3').replace('i', '1').replace('o', '0')
-    # Mostrar la clave en la etiqueta
-    clave1.config(text=url)
-    # Copiar la clave al portapapeles
-    copiar_al_portapapeles(url)
+    url = urlInput.get()
+    if url != '':
+        sshUrl = url.replace("https://", "").replace("http://", "").replace('/','')
+        str = sshUrl.replace('sis-', '').replace('siap-', '').replace('.salud.gob.sv','').replace('a', '4').replace('e', '3').replace('i', '1').replace('o', '0').replace('/','')
+
+        keyText1.config(text = 's1s'+str+'-s14p')
+        keyText2.config(text = 's14p'+str+'-s14p')
+        sshText.config(text = 'ssh siap@' + sshUrl)
+        btnCopiar1.config(state=tk.NORMAL)
+        btnCopiar2.config(state=tk.NORMAL)
+        btnSSH.config(state=tk.NORMAL)
+    else:
+        btnCopiar1.config(state=tk.DISABLED)
+        btnCopiar2.config(state=tk.DISABLED)
+        btnSSH.config(state=tk.DISABLED)
+
 
 # Función para copiar el texto al portapapeles
-def copiar_al_portapapeles(texto):
-    pyperclip.copy(texto)
-    messagebox.showinfo("Clave en portapapeles", "¡La clave se copió con éxito!")
+def copiarClave(valor):
+    if valor == 1:
+        clave = keyText1.cget('text')
+        btnCopiar1.config(state=tk.DISABLED)
+    elif valor == 2:
+        clave = keyText2.cget('text')
+        btnCopiar2.config(state=tk.DISABLED)
+    elif valor == 3:
+        clave = sshText.cget('text')
+        btnSSH.config(state=tk.DISABLED)
+    else:
+        clave = ""  # Definir un valor por defecto si valor no coincide con 1, 2 o 3
+
+    pyperclip.copy(clave)
 
 # Función para cerrar la aplicación
 def cerrar():
     root.destroy()
+
+#valores generales
+anchokey = 30
 
 # Crear la ventana principal
 root = tk.Tk()
@@ -61,24 +85,38 @@ etiqueta1 = tk.Label(root, text="Ingresa la URL")
 etiqueta1.grid(row=0, column=0, columnspan=2, padx=5, pady=5)
 
 # Campo de entrada en la fila 1, columna 0
-entrada = tk.Entry(root)
-entrada.grid(row=1, column=0, padx=5, pady=5)
+urlInput = tk.Entry(root, width = anchokey)
+urlInput.grid(row=1, column=0, padx=5, pady=5)
 
 # Botón en la fila 1, columna 1
-boton = tk.Button(root, text="Generar", command=generar_keys)
-boton.grid(row=1, column=1, padx=5, pady=5)
+botonGenerar = tk.Button(root, text="Generar", command=generar_keys)
+botonGenerar.grid(row=1, column=1, padx=5, pady=5)
 
-# Etiqueta para mostrar la clave generada
-clave1 = tk.Label(root, text="", font=("Arial", 14))
-clave1.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+#clave 1
+keyText1 = tk.Label(root, text="clave 1", font=("Arial", 9), width = anchokey, anchor="sw")
+keyText1.grid(row=2, column=0, padx=5, pady=5)
+btnCopiar1 = tk.Button(root, text="Copiar", command=lambda:copiarClave(1))
+btnCopiar1.grid(row=2, column=1, padx=5, pady=5)
+btnCopiar1.config(state=tk.DISABLED)
 
-# Botón para copiar la clave al portapapeles
-btnCopiar = tk.Button(root, text="Copiar", command=copiar_al_portapapeles)
-btnCopiar.grid(row=3, column=0, columnspan=2, pady=10)
+#clave 2
+keyText2 = tk.Label(root, text="clave 2", font=("Arial", 9), width = anchokey, anchor="sw")
+keyText2.grid(row=3, column=0, padx=0, pady=5)
+btnCopiar2 = tk.Button(root, text="Copiar", command=lambda:copiarClave(2))
+btnCopiar2.grid(row=3, column=1, padx=5, pady=5)
+btnCopiar2.config(state=tk.DISABLED)
+
+#SSH
+sshText = tk.Label(root, text="ssh", font=("Arial", 9), width = anchokey, anchor="sw")
+sshText.grid(row=4, column=0, padx=5, pady=5)
+btnSSH = tk.Button(root, text="Copiar", command=lambda:copiarClave(3))
+btnSSH.grid(row=4, column=1, padx=5, pady=5)
+btnSSH.config(state=tk.DISABLED)
+
 
 # Botón para cerrar la aplicación
 btnCerrar = tk.Button(root, text="Cerrar", command=cerrar)
-btnCerrar.grid(row=4, column=0, columnspan=2, pady=10)
+btnCerrar.grid(row=5, column=0, columnspan=2, pady=10)
 
 # Iniciar el bucle principal
 root.mainloop()
